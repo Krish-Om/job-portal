a3617439cf315c49fa3e0d465b367b995abed15c
Krishom <Krishombasukala@gmail.com>
2025-02-11 14:35:22 +0545

    Add dependency function to manage database sessions

---
+++ backend/dependencies.py
@@ -0,0 +1,18 @@
+from typing import Generator
+
+from sqlalchemy.orm import Session
+from .database import SessionLocal
+
+def get_db() -> Generator[Session,None,None]:
+    """
+    Dependency function to get a database session.
+
+    Yields:
+        Session: A database session.
+    """
+
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
